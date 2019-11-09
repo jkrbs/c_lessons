a72d921b68612b4ee59f2c7ffda5e15749a98cf0
Christian Schwarz <cmrs@cmrs.io>
2019-11-09 08:35:23 +0100

    add solutions for the first 3 tasks

--- build/build.sh
+++ build/build.sh
@@ -4,8 +4,11 @@ DIR="$(dirname "$(readlink -f "$0")")" #directory of shell script
 function build() {
     cd "$DIR/../$1/"
     mkdir -p "$DIR/$1"
+    # we need to call pdflatex twice to get the table of contents ... 
+    # damn this is ugly. breaks on @,&,|,\n or $ in path names
     find -maxdepth 1 -type f -iname '*.tex' -print0 | xargs -I@ -r -0 -n 1 sh -c "pdflatex -output-directory '$DIR/$1' @ && pdflatex -output-directory '$DIR/$1' @"
     find -maxdepth 1 -type f -iname '*.md' | sed "s|\\(.*\\)\\.md|pandoc '\\1.md' -o '$DIR/$1/\\1.pdf'|" | sh
 }
 build slides
 build tasks
+build task_solutions
