835f2add57e943628337a53402ac54ba354ffa23
Christian Schwarz <cmrs@cmrs.io>
2019-11-24 18:04:57 +0100

    add build all option

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
+
 DIR="$(dirname "$(readlink -f "$0")")" #directory of shell script
 function file_newer(){
     file_1_commit=$(git log --follow -n 1 --pretty=format:"%h" -- $1)
@@ -11,20 +12,33 @@ function build() {
     cd "$DIR/../$1/"
     mkdir -p "$DIR/$1"
     # we need to call pdflatex twice to get the table of contents ... 
-    for texfile in ./*.tex; do
+    for texfile in ./*.tex ; do
+        [ -e "$texfile" ] || continue
         filename="${texfile%.*}"
-        if file_newer "$texfile" "$DIR/$1/$filename.pdf"; then
-            pdflatex -output-directory "$DIR/$1" $texfile && 
-                pdflatex -output-directory "$DIR/$1" $texfile         
+        printf "\ncompiling $texfile ...\n"
+        if [ $build_all = true ] || file_newer "$texfile" "$DIR/$1/$filename.pdf"; then
+            pdflatex --interaction=batchmode -output-directory "$DIR/$1" $texfile && 
+                pdflatex --interaction=batchmode -output-directory "$DIR/$1" $texfile || :        
         fi
     done
-    for mdfile in ./*.md; do
+
+    for mdfile in ./*.md ; do
+        [ -e "$mdfile" ] || continue
+        echo "compiling $mdfile ..."
         filename="${mdfile%.*}"
-        if file_newer "$mdfile" -nt "$DIR/$1/$filename.pdf"; then
+        if [ $build_all = true ] || file_newer "$mdfile" -nt "$DIR/$1/$filename.pdf"; then
             pandoc "$mdfile" -o "$filename.pdf"
         fi
     done
 }
+
+build_all=false
+if [ "$#" -gt "0" ]; then
+    if [ "$1" == "-a" ] || [ "$1" == "--all" ] ; then
+        build_all=true
+    fi
+fi
+
 build slides
 build tasks
 build task_solutions
