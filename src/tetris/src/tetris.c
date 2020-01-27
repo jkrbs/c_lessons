5396a6bf0fb27d960ec3075ed2af5a75b1e5561f
Jakob Krebs <jakob@txrx23.de>
2020-01-27 14:21:12 +0100

    implement line clearing and fix bounds collision

--- src/tetris/src/tetris.c
+++ src/tetris/src/tetris.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
-#define BOARD_WIDTH  30
-#define BOARD_HEIGHT 24 
+#define BOARD_WIDTH  10
+#define BOARD_HEIGHT 20 
 
 
 enum block_type {
     BLOCK_L1, BLOCK_L2, BLOCK_Z1, BLOCK_Z2, BLOCK_I, BLOCK_T, BLOCK_Q, BLOCK_COUNT, BLOCK_NONE
 };
+
+typedef enum collision_type {COLLISION_NONE, COLLSION_BLOCK, COLLISION_BOTTOM, COLLISION_BORDER
+} collision_type;
 
 
 //TODO add missing blocks
@@ -72,43 +75,48 @@ struct gamestate {
 } gamestate;
 
 //returns true on collision
-bool char_insert(int xoffset, int yoffset, bool del) {
+collision_type char_insert(int xoffset, int yoffset, bool del) {
     int x = gamestate.cur_block.x + xoffset;
     int y = gamestate.cur_block.y + yoffset;
     char bc = block_symbols[gamestate.cur_block.kind][gamestate.cur_block.rotation][yoffset][xoffset];
 
-    if(x<0 || x>=BOARD_WIDTH || y>= BOARD_HEIGHT || y < 0) {
-        return bc != ' ';
+    if(x<0 || x>=BOARD_WIDTH || y < 0) {
+        return bc != ' ' ? COLLISION_BORDER : COLLISION_NONE;
     }
 
+    if( y>= BOARD_HEIGHT) {
+        return bc != ' ' ? COLLISION_BOTTOM : COLLISION_NONE;
+    }
+    
     if(gamestate.board[x][y] == ' '){
         if(!del) gamestate.board[x][y] = bc;
-        return false;
+        return COLLISION_NONE;
     }
     if(del){
         if(bc != ' ') gamestate.board[x][y] = ' ';
-        return false;
+        return COLLISION_NONE;
     }
-    return bc != ' ';
+    return bc != ' ' ? COLLSION_BLOCK : COLLISION_NONE;
 }
 
-bool block_insert(bool del) {
+collision_type block_insert(bool del) {
  
     enum block_type kind = gamestate.cur_block.kind;
 
     for(int i = 0; i<4; i++) {
         for(int j =0; j<4; j++) {
-            if(char_insert(i, j, del)){
+            collision_type ct = char_insert(i, j, del);
+            if(ct){
                 assert(!del);
                 for(int k = 0; k < i; k++){
                     for(int l = 0; l <  4; l++) char_insert(k, l, true);
                 }
                 for(int l = 0; l < j; l++) char_insert(i, l, true);
-                return true;
+                return ct;
             }
         }
     }
-    return false;
+    return COLLISION_NONE;
 }
 
 void move(int x, int y, int rot) {
@@ -117,26 +125,40 @@ void move(int x, int y, int rot) {
     gamestate.cur_block.x += x;
     gamestate.cur_block.y += y;
     gamestate.cur_block.rotation = (gamestate.cur_block.rotation + 4 + rot) % 4;
-    if(block_insert(false)) {
+    
+    collision_type ct =block_insert(false);
+    
+    if(ct) {
         gamestate.cur_block.x -= x;
         gamestate.cur_block.y -= y;
         gamestate.cur_block.rotation = rot_prev;
         block_insert(false);
-        gamestate.cur_block.kind = BLOCK_NONE;
+        if(ct == COLLSION_BLOCK || ct == COLLISION_BOTTOM) {
+            gamestate.cur_block.kind = BLOCK_NONE;
+        }
     }
 }
 
 void render_board() {
     termio_clear_screen();
-    printf("x: %i, y: %i, rot: %i, kind: %i\n",
-        gamestate.cur_block.x,gamestate.cur_block.y,gamestate.cur_block.rotation,gamestate.cur_block.kind
-    );
+
+    for(int i = 0; i< BOARD_WIDTH+2; i++) {
+        putchar('@');
+    }
+    putchar('\n');
+
     for(int y = 0; y < BOARD_HEIGHT; y++) {
+        putchar('@');
         for(int x = 0; x < BOARD_WIDTH; x++) {
             putchar(gamestate.board[x][y]);
         }
-        putchar('\n');
+        puts("@");
     }
+
+    for(int i = 0; i< BOARD_WIDTH+2; i++) {
+        putchar('@');
+    }
+    putchar('\n');
 }
 
 
@@ -157,8 +179,33 @@ void gametick() {
 
     while(true){
         if(gamestate.cur_block.kind == BLOCK_NONE) {
-            gamestate.cur_block.kind= rand() % BLOCK_COUNT;
 
+            for(int y = 0; y < BOARD_HEIGHT; y++) {
+                bool line_full = true;
+                for(int x = 0; x < BOARD_WIDTH; x++) {
+                    if(gamestate.board[x][y] == ' ') {
+                        line_full = false;
+                        break;
+                    }
+                }
+                if(line_full) {
+                    int line_y = y;
+                    for(int x = 0; x < BOARD_WIDTH; x++) {
+                        //memmove(&gamestate.board[x][y], &gamestate.board[x][y+1], sizeof(char)*y);
+                        for(int y = line_y; y >= 1; y--) {
+                            gamestate.board[x][y] = gamestate.board[x][y-1];
+                        }
+                    }
+                    for(int x = 0; x< BOARD_WIDTH; x++) {
+                        gamestate.board[x][0] = ' ';
+                    }
+                
+                }
+            }
+
+
+            gamestate.cur_block.kind= rand() % BLOCK_COUNT;
+            gamestate.cur_block.kind = BLOCK_I; //debug
             gamestate.cur_block.y = 0;
             gamestate.cur_block.x = BOARD_WIDTH/2;
 
@@ -200,7 +247,7 @@ int main() {
     {
         gametick();
         render_board();
-        usleep(500000);
+        usleep(300000);
     }
 
     termio_fin();
